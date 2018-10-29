64dc9e52009256f8bca8d55674f3814a91a90ea3
Himanshu Tiwari <himanshutiwari@Himanshus-MacBook-Air.local>
2018-10-29 18:53:08 +0530

    Added express as a dependency and created a server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
